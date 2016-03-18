331a7f2f80132afd0488b92cbb9f7af6f1ecf6ab
HarshEvilGeek <akhilcherian@gmail.com>
2016-03-18 14:09:01 +0530

    Basic outline and flow. Navigation drawer jumps between fragments

--- app/src/main/java/com/example/zaas/pocketbanker/activities/MainActivity.java
+++ app/src/main/java/com/example/zaas/pocketbanker/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.zaas.pocketbanker.activities;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +10,26 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.example.zaas.pocketbanker.R;
+import com.example.zaas.pocketbanker.fragments.ATMBranchLocatorFragment;
+import com.example.zaas.pocketbanker.fragments.AccountSummaryFragment;
+import com.example.zaas.pocketbanker.fragments.AnalyticsFragment;
+import com.example.zaas.pocketbanker.fragments.PocketsFragment;
+import com.example.zaas.pocketbanker.fragments.RecommendationsFragment;
+import com.example.zaas.pocketbanker.fragments.TransactionsFragment;
+import com.example.zaas.pocketbanker.fragments.TransferFundsFragment;
 
 public class MainActivity extends BaseRestrictedActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    FloatingActionButton fab;
+    FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +38,11 @@ public class MainActivity extends BaseRestrictedActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Random text", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -55,13 +68,6 @@ public class MainActivity extends BaseRestrictedActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -82,20 +88,27 @@ public class MainActivity extends BaseRestrictedActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        Fragment fragment = null;
+        if (id == R.id.account_summary) {
+            fragment = new AccountSummaryFragment();
+        } else if (id == R.id.transactions) {
+            fragment = new TransactionsFragment();
+        } else if (id == R.id.transfer_funds) {
+            fragment = new TransferFundsFragment();
+        } else if (id == R.id.atm_branch_locator) {
+            fragment = new ATMBranchLocatorFragment();
+        } else if (id == R.id.analytics) {
+            fragment = new AnalyticsFragment();
+        } else if (id == R.id.recommendations) {
+            fragment = new RecommendationsFragment();
+        } else if (id == R.id.pockets) {
+            fragment = new PocketsFragment();
         }
 
+        if (fragment != null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
--- app/src/main/java/com/example/zaas/pocketbanker/fragments/RecommendationsFragment.java
+++ app/src/main/java/com/example/zaas/pocketbanker/fragments/RecommendationsFragment.java
@@ -1,9 +1,28 @@
 package com.example.zaas.pocketbanker.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.zaas.pocketbanker.R;
 
 /**
  * Created by zaas on 3/17/16.
  */
 public class RecommendationsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_recommendations, container,false);
+        return rootView;
+    }
 }
