66e11a08422c0fe686bd6ae393be11f1408c5b96
HarshEvilGeek <akhilcherian@gmail.com>
2016-03-20 17:46:12 +0530

    more login changes

--- app/src/main/java/com/example/zaas/pocketbanker/fragments/LoginFragment.java
+++ app/src/main/java/com/example/zaas/pocketbanker/fragments/LoginFragment.java
@@ -1,9 +1,34 @@
 package com.example.zaas.pocketbanker.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.zaas.pocketbanker.R;
 
 /**
  * Created by akhil on 3/20/16.
  */
 public class LoginFragment extends Fragment {
+
+    EditText custIdField;
+    EditText passwordField;
+    Button loginButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container,false);
+        custIdField = (EditText) rootView.findViewById(R.id.customer_id);
+        passwordField = (EditText) rootView.findViewById(R.id.password);
+        loginButton = (Button) rootView.findViewById(R.id.login_button);
+
+
+        return rootView;
+    }
 }
