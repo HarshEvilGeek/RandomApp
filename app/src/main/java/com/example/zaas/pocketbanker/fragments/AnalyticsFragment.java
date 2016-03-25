bbc709d8c7b78c084f1333b1bea35e5bba35c0fa
shseth <shseth@microsoft.com>
2016-03-25 19:11:10 +0530

    Analytics UI complete

--- app/src/main/java/com/example/zaas/pocketbanker/fragments/AnalyticsFragment.java
+++ app/src/main/java/com/example/zaas/pocketbanker/fragments/AnalyticsFragment.java
@@ -1,129 +1,110 @@
 package com.example.zaas.pocketbanker.fragments;
 
 import android.app.Fragment;
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.example.zaas.pocketbanker.R;
-import com.example.zaas.pocketbanker.interfaces.IFloatingButtonListener;
+import com.example.zaas.pocketbanker.data.PocketBankerDBHelper;
+import com.example.zaas.pocketbanker.models.local.Transaction;
+import com.example.zaas.pocketbanker.utils.TransactionCategoryUtils;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
-import org.achartengine.ChartFactory;
-import org.achartengine.GraphicalView;
-import org.achartengine.model.CategorySeries;
-import org.achartengine.model.SeriesSelection;
-import org.achartengine.renderer.DefaultRenderer;
-import org.achartengine.renderer.SimpleSeriesRenderer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by zaas on 3/17/16.
  */
-public class AnalyticsFragment extends Fragment implements IFloatingButtonListener {
+public class AnalyticsFragment extends Fragment{
 
-    private static int[] COLORS = new int[] { Color.GREEN, Color.BLUE,Color.MAGENTA, Color.CYAN };
+    //private int[] mColors;
+            //new int[] { Color.GREEN, Color.BLUE, Color.YELLOW, Color.MAGENTA, Color.CYAN, Color.RED  };
 
-    private static double[] VALUES = new double[] { 10, 11, 12, 13 };
+    private static String[] CATEGORY_LABELS = Arrays.toString(TransactionCategoryUtils.Category.values()).replaceAll("^.|.$", "").split(", ");
 
-    private static String[] NAME_LIST = new String[] { "A", "B", "C", "D" };
-
-    private CategorySeries mSeries = new CategorySeries("");
-
-    private DefaultRenderer mRenderer = new DefaultRenderer();
-
-    private GraphicalView mChartView;
+    private HashMap<String, Double> mAmountPerCategory = new HashMap<>();
+    private PieDataSet mDataset;
+    private PieChart mPieChart;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeAmounts();
 
-        mRenderer.setApplyBackgroundColor(true);
-        mRenderer.setBackgroundColor(Color.argb(100, 50, 50, 50));
-        mRenderer.setChartTitleTextSize(20);
-        mRenderer.setLabelsTextSize(15);
-        mRenderer.setLegendTextSize(15);
-        mRenderer.setMargins(new int[]{20, 30, 15, 0});
-        mRenderer.setZoomButtonsVisible(true);
-        mRenderer.setStartAngle(90);
+        List<Transaction> allTransactions = PocketBankerDBHelper.getInstance().getAllTransactions();
+        categorizeTransactionAmounts(allTransactions);
 
-        for (int i = 0; i < VALUES.length; i++) {
-            mSeries.add(NAME_LIST[i] + " " + VALUES[i], VALUES[i]);
-            SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();
-            renderer.setColor(COLORS[(mSeries.getItemCount() - 1) % COLORS.length]);
-            mRenderer.addSeriesRenderer(renderer);
+        // creating data values
+        ArrayList<Entry> values = new ArrayList<>();
+        int k = 0;
+        for (Double amount : mAmountPerCategory.values()) {
+            values.add(new Entry(amount.floatValue(), k++));
         }
 
-
-
-        if (mChartView != null) {
-            mChartView.repaint();
-        }
+        mDataset = new PieDataSet(values, "Transaction Amounts");
+        // add many colors
+        ArrayList<Integer> colors = getPieColorPalette();
+        mDataset.setColors(colors);
+        mDataset.setSliceSpace(3f);
+        mDataset.setDrawValues(false);
     }
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_analytics, container,false);
-        LinearLayout layout = (LinearLayout) rootView.findViewById(R.id.chart);
-        mChartView = ChartFactory.getPieChartView(getActivity(), mSeries, mRenderer);
-        mRenderer.setClickEnabled(true);
-        mRenderer.setSelectableBuffer(10);
-
-        mChartView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SeriesSelection seriesSelection = mChartView.getCurrentSeriesAndPoint();
-
-                if (seriesSelection == null) {
-                    Toast.makeText(getActivity(), "No chart element was clicked", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(getActivity(),"Chart element data point index "+ (seriesSelection.getPointIndex()+1) + " was clicked" + " point value="+ seriesSelection.getValue(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        mChartView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                SeriesSelection seriesSelection = mChartView.getCurrentSeriesAndPoint();
-                if (seriesSelection == null) {
-                    Toast.makeText(getActivity(),"No chart element was long pressed", Toast.LENGTH_SHORT);
-                    return false;
-                }
-                else {
-                    Toast.makeText(getActivity(),"Chart element data point index "+ seriesSelection.getPointIndex()+ " was long pressed",Toast.LENGTH_SHORT);
-                    return true;
-                }
-            }
-        });
-        layout.addView(mChartView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+        View rootView = inflater.inflate(R.layout.fragment_analytics, container, false);
+        mPieChart = (PieChart) rootView.findViewById(R.id.chart);
+        mPieChart.setData(new PieData(CATEGORY_LABELS, mDataset));
+        updateChartUI();
 
         return rootView;
     }
 
-    @Override
-    public Drawable getFloatingButtonDrawable(Context context) {
-        return ContextCompat.getDrawable(context, R.drawable.plus_32);
+    private void updateChartUI() {
+        mPieChart.getLegend().setEnabled(false);
+        mPieChart.setCenterText("Transaction Analysis");
+        mPieChart.setDescription("");
+        mPieChart.setCenterTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        mPieChart.setDescriptionTextSize(15f);
+        mPieChart.animateY(1000);
     }
 
-    @Override
-    public void onFloatingButtonPressed() {
-        Toast.makeText(getActivity(), "BLET", Toast.LENGTH_LONG).show();
+    private void categorizeTransactionAmounts(List<Transaction> allTransactions) {
+        double amount;
+        for (Transaction currentTransaction : allTransactions) {
+            amount = mAmountPerCategory.get(currentTransaction.getCategory().name()).doubleValue();
+            amount = Double.valueOf(amount + currentTransaction.getAmount());
+            mAmountPerCategory.put(currentTransaction.getCategory().name(), amount);
+        }
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mChartView != null) {
-            mChartView.repaint();
+    private ArrayList<Integer> getPieColorPalette() {
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+        return colors;
+    }
+
+    private void initializeAmounts() {
+        for (String categoryString : CATEGORY_LABELS) {
+            mAmountPerCategory.put(categoryString, Double.valueOf(0));
         }
     }
 }
