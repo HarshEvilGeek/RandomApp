5c4b0625de5204e285f20245d20a7e5159c4806d
HarshEvilGeek <akhilcherian@gmail.com>
2016-03-27 15:55:38 +0530

    ranosdaudj

--- app/src/main/java/com/example/zaas/pocketbanker/adapters/PocketShopAdapter.java
+++ app/src/main/java/com/example/zaas/pocketbanker/adapters/PocketShopAdapter.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,12 +19,13 @@ import com.bumptech.glide.Glide;
 import com.example.zaas.pocketbanker.R;
 import com.example.zaas.pocketbanker.models.local.Shop;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by akhil on 3/26/16.
  */
-public class PocketShopAdapter extends RecyclerView.Adapter<PocketShopAdapter.PocketShopViewHolder>
+public class PocketShopAdapter extends RecyclerView.Adapter<PocketShopAdapter.PocketShopViewHolder> implements Filterable
 {
     private Context mContext;
     private LayoutInflater mInflater;
@@ -97,6 +100,45 @@ public class PocketShopAdapter extends RecyclerView.Adapter<PocketShopAdapter.Po
     public int getItemCount()
     {
         return mShopList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+
+        Filter filter = new Filter() {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                mShopList = (List<Shop>) results.values;
+                notifyDataSetChanged();
+            }
+
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+
+                FilterResults results = new FilterResults();
+                List<Shop> filteredShops = new ArrayList<>();
+
+                // perform your search here using the searchConstraint String.
+
+                constraint = constraint.toString().toLowerCase();
+                for (int i = 0; i < mShopList.size(); i++) {
+                    String shopName = mShopList.get(i).getName();
+                    if (shopName.toLowerCase().startsWith(constraint.toString()))  {
+                        filteredShops.add(mShopList.get(i));
+                    }
+                }
+
+                results.count = filteredShops.size();
+                results.values = filteredShops;
+                Log.e("VALUES", results.values.toString());
+
+                return results;
+            }
+        };
+
+        return filter;
     }
 
     public class PocketShopViewHolder extends RecyclerView.ViewHolder
